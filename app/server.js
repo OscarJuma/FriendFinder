2daf8f96c4943574aa8944cdb3246470aaad86da
Oscar Jums <oscarjuma2004dr@yahoo.com>
2018-11-18 08:32:07 -0600

    links
    fixed routing errors in server.js

--- app/server.js
+++ app/server.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('app'));
 
-require('./app/routing/apiRoutes.js')(app); 
-require('./app/routing/htmlRoutes.js')(app);
+require('./routing/apiRoutes.js')(app); 
+require('./routing/htmlRoutes.js')(app);
 
 
 app.listen(process.env.PORT || 8000);
