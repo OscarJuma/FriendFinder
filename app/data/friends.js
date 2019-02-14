3bc0566c7c996060cf76ae91940a3701af168194
Oscar Jums <oscarjuma2004dr@yahoo.com>
2019-02-14 11:47:12 -0600

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -30,4 +30,41 @@ var friends = [{
 		1,
 		1
 	]
-}];
+},
+
+{
+	"name": "Hawa",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"scores": [
+		1, 
+		2, 
+		1, 
+		1, 
+		5, 
+		1, 
+		2, 
+		1, 
+		4,
+		1
+	]
+},
+
+{
+	"name": "Huyu",
+	"photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"scores": [
+		5, 
+		1, 
+		2, 
+		1, 
+		1, 
+		4, 
+		4, 
+		1, 
+		2,
+		1
+	]
+}
+];
+
+module.exports = friends;
